2094ac14f7ddb08e556f31d4f41f5dc70247ed76
Daniel <danielsolano1000@yahoo.com.br>
2020-06-30 16:02:35 -0300

    Aprimoramento das requisições

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -10,13 +10,22 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes"); // Especifica o método e URL
 
     xhr.addEventListener("load", function(){ // Coleta a resposta
-        var resposta = xhr.responseText; // Resposta vem como string
-        var pacientes = JSON.parse(resposta); // Lê o arquivo JSON e transforma num objeto Javascript
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (xhr.status == 200) { // Verifica se a conexão está ok
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText; // Resposta vem como string
+            var pacientes = JSON.parse(resposta); // Lê o arquivo JSON e transforma num objeto Javascript
         
-        pacientes.forEach(function(paciente){ // Outra forma de se usar o loop for
+            pacientes.forEach(function(paciente){ // Outra forma de se usar o loop for
             adicionaPacienteNaTabela(paciente); // Chama a função que adiciona na tabela
-        });
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);            
+            erroAjax.classList.remove("invisivel"); // Torna a mensagem de erro visível
+        }
+        
     });
 
-    xhr.send(); // Faz a requisição 
+    xhr.send(); // Envia a requisição 
 });
